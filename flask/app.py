5f27d66d26a8f7820af32d360203f0a1922b1de2
Ayşe Ucmaklı <ucmakliayse@gmail.com>
2024-01-16 23:30:31 +0300

    updated app.py

--- flask/app.py
+++ flask/app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    test_np_input = np.array([[1],[2],[17]])
+    model = load('model.joblib')
+    preds = model.predict(test_np_input)
+    preds_as_str = str(preds)
+    return preds_as_str'
