c203d3a819c3100f429b98aa0ddc8f67722c627a
Aysucmkli <108420703+AyseUcmakli@users.noreply.github.com>
2024-01-20 15:23:07 +0300

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
     test_np_input = np.array([[1],[2],[17]])
     model = load('model.joblib')
